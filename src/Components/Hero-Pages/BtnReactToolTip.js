7e213766d885c1260203e48d7c04a9e17bbe1dd1
Romin K <37375864+Rominkc@users.noreply.github.com>
2019-06-11 22:03:23 -0400

    Increased font size of tooltips

--- src/Components/Hero-Pages/BtnReactToolTip.js
+++ src/Components/Hero-Pages/BtnReactToolTip.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactToolTip from 'react-tooltip'
-
+//css in Hero-Pages.css
 const BtnReactToolTip =(props)=>(
     <>
     <ReactToolTip {...props} place="right" className='react-tool-tip-btn'
