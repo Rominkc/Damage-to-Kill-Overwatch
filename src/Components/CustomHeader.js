bc65fabc5276b622946a1f62c2bd07b259ae9d49
Romin K <37375864+Rominkc@users.noreply.github.com>
2019-09-15 18:33:24 -0400

    Updated background of various components to which subjectively made UI more pleasing to look at

--- src/Components/CustomHeader.js
+++ src/Components/CustomHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import './CustomHeader.css'
-const CustomHeader= () =>(
-    <div className="header-dmg2kOW" style={{textAlign:"left"}} >
-    <Link to='/' className="header-link-dmg2kOW">
-    <span> Damage to Kill Overwatch </span>
+import { Link } from 'react-router-dom';
+import './CustomHeader.css';
+const CustomHeader = () => (
+  <div className='header-dmg2kOW main-header'>
+    <Link to='/' className='header-link-dmg2kOW'>
+      <span> Damage to Kill Overwatch </span>
     </Link>
-    </div>
+  </div>
 );
 
 export default CustomHeader;
